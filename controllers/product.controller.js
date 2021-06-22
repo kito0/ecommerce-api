76e90b0ec55ed0a829cc7fc8e566f5553c9138d4
kito0 <drewshah0@gmail.com>
2021-06-22 15:50:11 -0600

    Add endpoints for fetching product by id, editing, and deleting products

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,23 +2,62 @@ const Product = require('../models/product.model');
 
 // GET http://localhost:5000/api/v1/products
 exports.getAllProducts = async (req, res) => {
-	Product.find().then((products) => {
-		res.status(200).json(products);
-	});
+	Product.find()
+		.then((products) => {
+			res.status(200).json(products);
+		})
+		.catch((err) => res.status(500).send(err));
 };
 
-// GET http://localhost:5000/api/v1/products
+// GET http://localhost:5000/api/v1/products/:id
+exports.getProductByID = async (req, res) => {
+	Product.findById(req.params.id)
+		.then((product) => {
+			res.status(200).json(product);
+		})
+		.catch((err) => res.status(500).send(err));
+};
+
+// POST http://localhost:5000/api/v1/products
 exports.createProduct = async (req, res) => {
-	Product.create(req.body).then((product) => {
-		res.status(201).json(product);
-	});
+	Product.create(req.body)
+		.then((product) => {
+			res.status(201).json(product);
+		})
+		.catch((err) => res.status(400).send(err));
 };
 
 // PUT http://localhost:5000/api/v1/products/:id
 exports.editProduct = async (req, res) => {
-	Product.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
-		(product) => {
-			res.status(200).json(product);
-		}
-	);
+	Product.findByIdAndUpdate(req.params.id, req.body, { new: true })
+		.then((product) => {
+			product
+				? res.status(200).json(product)
+				: res.status(404).send('Product does not exist');
+		})
+		.catch((err) => res.status(500).send(err));
+};
+
+// PUT http://localhost:5000/api/v1/products/:id
+exports.addReview = async (req, res) => {
+	Product.findByIdAndUpdate(
+		req.params.id,
+		{ $push: { reviews: req.body } },
+		{ new: true }
+	)
+		.then((product) => {
+			res.status(201).json(product);
+		})
+		.catch((err) => res.status(400).send(err));
+};
+
+// DELETE http://localhost:5000/api/v1/products/:id
+exports.deleteProduct = async (req, res) => {
+	Product.findByIdAndDelete(req.params.id)
+		.then((product) => {
+			product
+				? res.status(200).json(product)
+				: res.status(404).send('Product does not exist');
+		})
+		.catch((err) => res.status(500).send(err));
 };
--- routes/product.route.js
+++ routes/product.route.js
@@ -2,14 +2,25 @@ const router = require('express').Router();
 
 const {
 	getAllProducts,
+	getProductByID,
 	createProduct,
 	editProduct,
+	addReview,
+	deleteProduct,
 } = require('../controllers/product.controller');
 
+// FETCH
 router.get('/', getAllProducts);
+router.get('/:id', getProductByID);
 
-router.put('/:id', editProduct);
-
+// CREATE
 router.post('/', createProduct);
+
+// UPDATE
+router.put('/:id', editProduct);
+router.put('/review/:id', addReview);
+
+// DELETE
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
